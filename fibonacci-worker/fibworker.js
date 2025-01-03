611c3991faed58c6c7519e552ce37c2ce602fe38
mehran <m.saghebi.dev@gmail.com>
2025-01-03 16:49:30 +0330

    working: :fire:  fibonacci progressbar

--- fibonacci-worker/fibworker.js
+++ fibonacci-worker/fibworker.js
@@ -1,41 +1,28 @@
-
 const arr = new Set();
 
-
 function fibonacci(n) {
-    arr.add(n)
+  arr.add(n);
 
-    const z = Array.from(arr).sort(function (a, b) { return b - a });
+  const z = Array.from(arr).sort(function (a, b) {
+    return b - a;
+  });
 
+  console.log("fib - xxx -- ", z);
 
-    console.log(z);
+  self.postMessage({ type: "progress", calculated: z[0] - 1 });
+  self.postMessage({ type: "sub-progress", calculating: n });
 
+  if (n <= 1) return n;
 
-    self.postMessage({ type: "progress", calculated: z[0] - 1 })
-    self.postMessage({ type: "sub-progress", calculating: n })
-
-
-
-
-    if (n <= 1) return n;
-
-    // Recursive case
-    return fibonacci(n - 1) + fibonacci(n - 2);
+  // Recursive case
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-
 
 self.onmessage = function (event) {
-    console.time("start")
+  console.time("start");
 
-    fibonacci(event.data)
-    self.postMessage({ type: "finished" })
+  fibonacci(event.data);
+  self.postMessage({ type: "finished" });
 
-    console.timeEnd("start")
-
-}
-
-
-
-
-
+  console.timeEnd("start");
+};
--- fibonacci-worker/script.js
+++ fibonacci-worker/script.js
@@ -1,83 +1,60 @@
-
 const worker = new Worker("fibworker.js");
-const fastWorker = new Worker("fastFibworker.js");
+const fastWorker = new Worker("fastFibonacciWorker.js");
 
-
-const numberInput = document.getElementById("inputNumber")
-const fastNumberInput = document.getElementById("fastInputNumber")
-const progressbarEl = document.getElementById("progressbar")
-const fastProgressbarEl = document.getElementById("fast-progressbar")
-const subProgressbarEl = document.getElementById("sub-progressbar")
-const countSpan = document.getElementById("count")
-const fastCountSpan = document.getElementById("fastCount")
-const subCountSpan = document.getElementById("sub-count")
-
-
+const numberInput = document.getElementById("inputNumber");
+const fastNumberInput = document.getElementById("fastInputNumber");
+const progressbarEl = document.getElementById("progressbar");
+const fastProgressbarEl = document.getElementById("fast-progressbar");
+const subProgressbarEl = document.getElementById("sub-progressbar");
+const countSpan = document.getElementById("count");
+const fastCountSpan = document.getElementById("fastCount");
+const subCountSpan = document.getElementById("sub-count");
 
 const calcFib = () => {
-    console.log(numberInput.value);
+  console.log(numberInput.value);
 
-    worker.postMessage(numberInput.value)
-
-
-}
+  worker.postMessage(numberInput.value);
+};
 
 const fastCalcFib = () => {
-    console.log(fastNumberInput);
+  console.log(fastNumberInput);
 
-    fastWorker.postMessage(fastNumberInput.value)
-
-}
+  fastWorker.postMessage(fastNumberInput.value);
+};
 
 worker.onmessage = function (event) {
-    console.log("[script.js]", event.data);
+  console.log("[script.js]", event.data);
 
-    if (event.data.type === "progress") {
-        countSpan.innerHTML = event.data.calculated
-        progressbarEl.value = (event.data.calculated / numberInput.value) * 1
-        //self.postMessage({ type: "progress", calculated: n - 1 })
-    }
-    if (event.data.type === "sub-progress") {
-        subCountSpan.innerHTML = event.data.calculating
-        subProgressbarEl.value = event.data.calculating
-        //self.postMessage({ type: "progress", calculated: n - 1 })
-    }
-    if (event.data.type === "finished") {
-        countSpan.innerHTML = "done"
-        progressbarEl.value = 100
+  if (event.data.type === "progress") {
+    countSpan.innerHTML = event.data.calculated;
+    progressbarEl.value = (event.data.calculated / numberInput.value) * 1;
+    //self.postMessage({ type: "progress", calculated: n - 1 })
+  }
+  if (event.data.type === "sub-progress") {
+    subCountSpan.innerHTML = event.data.calculating;
+    subProgressbarEl.value = event.data.calculating;
+    //self.postMessage({ type: "progress", calculated: n - 1 })
+  }
+  if (event.data.type === "finished") {
+    countSpan.innerHTML = "done";
+    progressbarEl.value = 100;
 
-        subCountSpan.innerHTML = "done!"
-        subProgressbarEl.value = 100
-
-    }
-
-
-}
+    subCountSpan.innerHTML = "done!";
+    subProgressbarEl.value = 100;
+  }
+};
 fastWorker.onmessage = function (event) {
+  if (event.data.type === "progress") {
+    fastCountSpan.innerHTML = event.data.calculated;
+    fastProgressbarEl.value = (event.data.calculated / numberInput.value) * 1;
+  }
 
-
-    if (event.data.type === "progress") {
-        fastCountSpan.innerHTML = event.data.calculated
-        fastProgressbarEl.value = (event.data.calculated / numberInput.value) * 1
-
-    }
-
-    if (event.data.type === "finished") {
-        fastCountSpan.innerHTML = "done"
-        fastProgressbarEl.value = 100
-
-
-    }
-
-
-}
-
-
-
-
-
+  if (event.data.type === "finished") {
+    fastCountSpan.innerHTML = "done";
+    fastProgressbarEl.value = 100;
+  }
+};
 
 const stopHandler = () => {
-    worker.postMessage("stop")
-
-}
+  worker.postMessage("stop");
+};
